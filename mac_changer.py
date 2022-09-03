08c1353eda0c375902ff64571f5cd273e7400c67
Kevin Cochrane <kevin.cochrane@gmail.com>
2022-09-03 09:59:07 +0200

    Added some comments.

--- mac_changer.py
+++ mac_changer.py
@@ -4,6 +4,7 @@ import subprocess
 import optparse
 import re
 
+
 def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface whose MAC Address you wish to change")
@@ -15,20 +16,22 @@ def get_arguments():
         parser.error("[-] Please specify a new MAC Address, use --help for more info")
     return options
 
-def change_mac(interface, new_mac):
+
+def change_mac(interface, new_mac):                                                                     # Run set of commands to change the MAC
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
-    # subprocess.call(["ifconfig", interface])
+
 
 def get_current_mac(interface):
-    ifconfig_result = subprocess.check_output(["ifconfig", interface])
-    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])                                  # Running ifconfig for specified interface
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))       # Using Regex to find the MAC address in the ifconfig result
     if mac_address_search_result:
         return mac_address_search_result.group(0)
     else:
         print("[-] Could not read MAC address.")
+
 
 options = get_arguments()
 current_mac = get_current_mac(options.interface)
@@ -36,7 +39,7 @@ print("Current MAC = " + str(current_mac))
 
 change_mac(options.interface, options.new_mac)
 
-current_mac =get_current_mac(options.interface)
+current_mac = get_current_mac(options.interface)
 if current_mac == options.new_mac:
     print("[+] MAC address was successfully changed to " + current_mac)
 else:
